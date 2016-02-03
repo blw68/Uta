8e150842939053352f39f9e37378beaab1d68635
blw68 <blw68@scarletmail.rutgers.edu>
2016-02-03 00:27:05 -0500

    Update Control.java

--- Control.java
+++ Control.java
@@ -1,5 +1,20 @@
-package Control;
+package Song;
 
-public class Control {
-
+public class Song {
+	public String songName;
+	public String artist;
+	public String album;
+	public int year;
+	
+	public Song(String songName, String artist, String album, int year) {
+		this.songName = songName;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;		
+	}
+	
+	public String toString() {
+		return songName + ", " + artist + ", " + album + ", " + year;
+	}
 }
+
