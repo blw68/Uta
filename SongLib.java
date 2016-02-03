8c3847a9106b20ba0446a8c27c7f030ffcf0a9d9
blw68 <blw68@scarletmail.rutgers.edu>
2016-02-02 21:09:17 -0500

    Create SongLib.java

---
+++ SongLib.java
@@ -0,0 +1,16 @@
+package SongLib;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SongLib extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
