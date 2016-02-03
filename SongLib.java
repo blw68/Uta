e0ee3b128fed9b79faf93671c134a3fa623a89ce
blw68 <blw68@scarletmail.rutgers.edu>
2016-02-03 00:27:31 -0500

    Update SongLib.java

--- SongLib.java
+++ SongLib.java
@@ -1,5 +1,9 @@
 package SongLib;
 
+import java.util.ArrayList;
+
+import Control.Control;
+import Song.Song;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -11,6 +15,17 @@ public class SongLib extends Application {
 	}
 
 	public static void main(String[] args) {
-		launch(args);
+		//launch(args);
+		
+		// just used to test add
+		System.out.println("add test");
+		ArrayList<Song> songList = new ArrayList<Song>();
+		Control.addInAbcOrder(songList, new Song("Ode to Joy", "Beethoven", "Ninth Symphony", 1785));
+		Control.addInAbcOrder(songList, new Song("Ode 2 Joy", "Beethoven", "Ninth Symphony", 1785));
+		Control.addInAbcOrder(songList, new Song("Ode 3 Joy", "Beethoven", "Ninth Symphony", 1785));
+		Control.addInAbcOrder(songList, new Song("Ode 1 Joy", "Beethoven", "Ninth Symphony", 1785));
+		Control.addInAbcOrder(songList, new Song("Ode 1 Joy", "Beethoven", "Ninth Symphony", 1785));
+		Control.addInAbcOrder(songList, new Song("Ode to Joy", "Beethoven", "Ninth Symphony", 1785));
+		Control.printList(songList);
 	}
 }
