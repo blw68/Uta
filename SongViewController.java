9c35dfedde499f084f551c9ee0dc3e3a11d7df13
snakebyte72 <laszlo.glant@gmail.com>
2016-02-06 21:17:28 -0500

    Update SongViewController.java

--- SongViewController.java
+++ SongViewController.java
@@ -1,13 +1,20 @@
 package SongLibView;
 
+import java.awt.TextField;
 import java.io.IOException;
 
+
 import app.SongLib;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class SongViewController {
 	
 	private SongLib songlib;
+	@FXML
+	public Button cancelButton;
 	
 	@FXML
 	private void homeScene() throws IOException{
@@ -16,6 +23,30 @@ public class SongViewController {
 	
 	@FXML
 	private void addButton() throws IOException{
-		songlib.showAddScene();
+		SongLib.showAddScene();
 	}
+	
+	@FXML
+	private void editButton() throws IOException{
+		SongLib.showEditScene();
+	}
+	
+	@FXML
+	private void deleteButton() throws IOException{
+		SongLib.showDeleteScene();
+	}
+	@FXML
+	private void cancelButtonEvent(ActionEvent event) {
+		Stage stage = (Stage) cancelButton.getScene().getWindow();
+	    stage.close();
+	}
+	
+	@FXML
+	private TextField title;
+	@FXML
+	private TextField artist;
+	@FXML
+	private TextField album;
+	@FXML
+	private TextField year;
 }
